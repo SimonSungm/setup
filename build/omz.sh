2e06764809ee1b14390d3958c32c830aba4b8e38
SimonSungm <simonsungm@gmail.com>
2022-12-17 17:27:55 +0000

    fix bug

--- build/omz.sh
+++ build/omz.sh
@@ -6,6 +6,5 @@ export CUR_DIR=$(dirname $(realpath -s "$0"))
 source ${CUR_DIR}/env.sh
 
 echo "Y" | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo 'export CUR_DIR=$(dirname $(realpath -s "$0"))' >> ${HOME}/.zshrc
 git config --global --add oh-my-zsh.hide-status 1
 git config --global --add oh-my-zsh.hide-dirty 1
