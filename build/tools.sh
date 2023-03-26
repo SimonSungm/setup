a60799e685748e8e9de3eb03704f79372072b0c7
SimonSungm <simonsungm@gmail.com>
2023-03-26 07:37:48 +0000

    add ctags

--- build/tools.sh
+++ build/tools.sh
@@ -12,3 +12,9 @@ sudo apt-get install -y \
 
 
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
+
+UCTAGS="uctags-2023.03.21-linux-`arch`"
+wget https://github.com/universal-ctags/ctags-nightly-build/releases/download/2023.03.21%2B8c18dabfa03da55b56b332f5c284fb7a236b6144/$UCTAGS.tar.xz -O /tmp/ctags.tar.xz
+tar -xvf /tmp/ctags.tar.xz -C /tmp/
+sudo cp /tmp/$UCTAGS/bin/* /usr/bin/
+
